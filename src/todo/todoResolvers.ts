f39687cf2f010c32b96c9a96aff743a13e91da3e
karim-hammami <hammamik790@gmail.com>
2023-01-29 15:45:53 +0100

    completed all queries and resolvers

--- src/todo/todoResolvers.ts
+++ src/todo/todoResolvers.ts
@@ -5,6 +5,15 @@ export default {
     Query: {
         allTodos: () => {
             return context.prisma.todo.findMany();
+        },
+        todoById: async (_: any, args: {id: number}) => {
+            const {id} = args;
+            const list = await context.prisma.todo.findUnique({
+                where: {
+                    id,
+                }
+            })
+            return list
         }
     },
     Mutation: {
@@ -17,6 +26,29 @@ export default {
                 }
             })
             return post
+        },
+        updateTodo: async (_: any, args: {id: number,title: string,desc: string,status: string}) => {
+            const {id, title, desc, status} = args
+            const update = await context.prisma.todo.update({
+                where: {
+                    id,
+                },
+                data: {
+                    title,
+                    desc,
+                    status,
+                }
+            })
+            return update
+        },
+        deleteTodo: async (_: any, args: {id: number}) => {
+            const {id} = args
+            const deletetodo = await context.prisma.todo.delete({
+                where: {
+                    id,
+                }
+            })
+            return deletetodo
         }
     }
 }
--- src/todo/todoTypeDefs.ts
+++ src/todo/todoTypeDefs.ts
@@ -12,9 +12,12 @@ export default gql`
 
     type Query {
         allTodos: [Todo]
+        todoById(id: Int): Todo
     }
 
     type Mutation {
         createTodo(title: String, desc: String): Todo
+        updateTodo(id: Int, title: String, desc: String, status: String): Todo 
+        deleteTodo(id: Int): Todo
     }
 `
